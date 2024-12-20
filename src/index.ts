d2e63b59345483c0785bbf2565e29d7a3669a222
Shan Amin <robin_zill@yahoo.com>
2024-12-20 23:56:07 +0500

    implementing summary and translate model from cloudflare workers AI

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World shan!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*', // '/*' means that the following rules apply to all the routes we have in this app
+	cors({
+		origin: '*', // Allow requests from your Nextjs app
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // Add Content-Type to the allowed headers to fix CORS
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/translateDocument', async (c) => {
+	const { documentData, targetlang } = await c.req.json();
+
+	// Generate summary of the document
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
+
+	// Translate the summary into another language
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english', // defaults to english
+		target_lang: targetlang,
+	});
+
+	return new Response(JSON.stringify(response));
+});
+
+export default app;
